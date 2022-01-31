59db497d72d08837bcf0446424adf8d869629178
Ojeola Ayofe <ojeolaponle@gmail.com>
2022-01-31 19:30:41 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleupdate(){
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+
+}
+inputs.forEach(input => input.addEventListener('change',handleupdate));
+
+inputs.forEach(input => input.addEventListener('mousemove',handleupdate));
